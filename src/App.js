1fe0e8934e1230a8cfe96199f97f54527cccb3e0
viraj1607 <merai.viraj@gmail.com>
2024-06-03 21:41:53 -0400

    react-router-dom setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+  ]);
+  return <div className="App">
+    <RouterProvider router={appRouter} />
+  </div>;
 }
 
 export default App;
