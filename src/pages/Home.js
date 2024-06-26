9c579ed1a89f7be2994aeb3391c374d262847f57
dhruv <dpatel7090@conestogac.on.ca>
2024-06-26 00:18:06 -0400

    made go button component, completed SearchDisplay component, StackedCard component, and AdBanner component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import SearchDisplay from '../components/SearchDisplay';
-
+import GoButton from '../components/GoButton';
+import AdBanner from '../components/AdBanner'; // Import the AdBanner component
+import StackedCard from '../components/StackedCard'; // Import the StackedCard component
 
 function Home() {
   return (
-    <div className="container mx-auto p-4">
-        <SearchDisplay />
-      </div>
-  )
+    <div className="container">
+      
+      <SearchDisplay />
+      {/* <div className="flex justify-center mt-8">
+        <GoButton />
+      </div> */}
+      <AdBanner imageUrl="/path/to/ad-image.jpg" /> {/* Use the AdBanner component */}
+      <StackedCard /> {/* Include the StackedCard component */}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
