ab33188577cb2ba4041aaf2134351ad04fc10d88
viraj1607 <merai.viraj@gmail.com>
2024-07-01 00:41:50 -0400

    fixed react-router-dom structrue

--- src/App.js
+++ src/App.js
@@ -1,37 +1,40 @@
 import React from 'react';
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
-import Header from "./components/Header";
 import Admin from "./pages/Admin";
-import Footer from "./components/Footer"; // Import the Footer component
 import HotelList from "./pages/HotelList";
 import UserProfile from "./pages/UserProfile";
+import Layout from './pages/Layout';
 
 function App() {
   const appRouter = createBrowserRouter([
     {
       path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/admin/:id",
-      element: <Admin />,
-    },
-    {
-      path: "/hotellist",
-      element: <HotelList />,
-    },
-    {
-      path: "userprofile",
-      element: <UserProfile />,
+      element: <Layout />, 
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/admin/:id",
+          element: <Admin />,
+        },
+        {
+          path: "/hotellist",
+          element: <HotelList />,
+        },
+        {
+          path: "/userprofile",
+          element: <UserProfile />,
+        },
+      ],
     },
   ]);
 
   return (
     <div className="App">
-      <Header />
       <RouterProvider router={appRouter} />
-      <Footer /> {/* Include the Footer component */}
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Logo from "../imgs/logo.png";
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div className='bg-[#002475] p-4'>
-        <img className='h-14 cursor-pointer' src={Logo} alt="Logo" />
+        <Link to="/"><img className='h-14 cursor-pointer' src={Logo} alt="Logo" /></Link>
     </div>
   );
 }
