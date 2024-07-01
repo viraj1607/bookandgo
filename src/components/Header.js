173ce20bd411b0d6fe8cd40d35a1280788df4f5a
dhruv <dpatel7090@conestogac.on.ca>
2024-07-01 17:21:24 -0400

    made hero section, and header container

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Logo from "../imgs/logo.png";
+import ProfileIcon from "./icons/profile.svg"; // Adjust the path if necessary
 
 const Header = () => {
   return (
-    <div className='bg-[#002475] p-4'>
+    <div className='header bg-[#002475] p-4 flex items-center justify-between'>
         <img className='h-14 cursor-pointer' src={Logo} alt="Logo" />
+        <img className='h-10 cursor-pointer' src={ProfileIcon} alt="Profile" />
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import SearchDisplay from '../components/SearchDisplay';
+import HeroSection from '../components/HeroSection';
 import GoButton from '../components/GoButton';
 import AdBanner from '../components/AdBanner'; // Import the AdBanner component
 import StackedCard from '../components/StackedCard'; // Import the StackedCard component
 import Offers from '../components/Offers'; // Import the Offers component
 import Locations from '../components/Locations'; // Import the Locations component
+import HeaderContainer from '../components/HeaderContainer';
 
 function Home() {
   return (
     <div className="container">
+      <HeroSection />
+      <HeaderContainer />
       <SearchDisplay />
       {/* <div className="flex justify-center mt-8">
         <GoButton />
