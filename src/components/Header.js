682fe95717e613caa41d7a04e052277150ef6425
viraj1607 <merai.viraj@gmail.com>
2024-07-02 12:11:36 -0400

    conflict resolved in header section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,17 @@
-import React from 'react';
+import React from "react";
 import Logo from "../imgs/logo.png";
-import ProfileIcon from "./icons/profile.svg"; // Adjust the path if necessary
+import ProfileIcon from "./icons/profile.svg";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className='header bg-[#002475] p-4 flex items-center justify-between'>
-        <img className='h-14 cursor-pointer' src={Logo} alt="Logo" />
-        <img className='h-10 cursor-pointer' src={ProfileIcon} alt="Profile" />
-import { Link } from 'react-router-dom';
-
-const Header = () => {
-  return (
-    <div className='bg-[#002475] p-4'>
-        <Link to="/"><img className='h-14 cursor-pointer' src={Logo} alt="Logo" /></Link>
+    <div className="header bg-[#002475] p-4 flex items-center justify-between">
+      <Link to="/">
+        <img className="h-14 cursor-pointer" src={Logo} alt="Logo" />
+      </Link>
+      <img className="h-10 cursor-pointer" src={ProfileIcon} alt="Profile" />
     </div>
   );
-}
+};
 
 export default Header;
