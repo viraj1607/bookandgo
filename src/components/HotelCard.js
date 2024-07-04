4da76ba55ad8998fe19d62cc0827502524c708ac
viraj1607 <merai.viraj@gmail.com>
2024-07-04 08:48:31 -0400

    Hotel Card

--- src/components/HotelCard.js
+++ src/components/HotelCard.js
@@ -19,14 +19,14 @@ const HotelCard = ({ hotelData = {} }) => {
   // }
 
   return (
-    <Link 
-      to="/hoteldetails" 
-      state={{ hotelData }} 
-    >
-      <div className="flex flex-col md:flex-row h-fit shadow-custom-grey p-4 my-8 mx-4 md:mx-16 rounded-xl">
+    <Link to="/hoteldetails" state={{ hotelData }}>
+      <div className="flex flex-col md:flex-row  shadow-custom-grey p-4 my-8 mx-4 md:mx-16 rounded-xl">
         <div>
-          <img className="rounded-xl" src={img} alt={hotelName} />
-          <img className="rounded-xl w-full md:w-auto" src={img} />
+          <img
+            className="rounded-xl w-full md:w-auto"
+            src={img}
+            alt={hotelName}
+          />
         </div>
         <div className="md:ml-4 mt-4 md:mt-0">
           <div className="flex flex-col md:flex-row justify-between mb-2">
@@ -41,12 +41,6 @@ const HotelCard = ({ hotelData = {} }) => {
                 +taxes <br /> per night
               </span>
             </div>
-      <div className="md:ml-4 mt-4 md:mt-0">
-        <div className="flex flex-col md:flex-row justify-between mb-2">
-          <div>
-            <h2 className="font-bold text-2xl md:text-[35px]">{hotelName}</h2>
-            <p className="text-[#6F6F6F] text-lg md:text-[20px]">{city}</p>
-            <span className="text-[#00B0F3]">{landmark}</span>
           </div>
           <Rating
             name="half-rating-read"
@@ -56,14 +50,15 @@ const HotelCard = ({ hotelData = {} }) => {
           />
           <p>{description}</p>
           <div className="flex flex-wrap">
-            {aminities && aminities.map((val, ind) => (
-              <span
-                key={ind}
-                className="border-grey border-2 px-2 m-2 rounded-lg"
-              >
-                {val}
-              </span>
-            ))}
+            {aminities &&
+              aminities.map((val, ind) => (
+                <span
+                  key={ind}
+                  className="border-grey border-2 px-2 m-2 rounded-lg"
+                >
+                  {val}
+                </span>
+              ))}
           </div>
         </div>
       </div>
