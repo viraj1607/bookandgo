8b97866a0bf3618213aff4c95356c95356266069
Venison Damesha <venisonsdamesa999@gmail.com>
2024-07-03 20:35:01 -0400

    Added Recommend and most view components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,28 @@
+// home.js
 import React from 'react';
 import SearchDisplay from '../components/SearchDisplay';
 import HeroSection from '../components/HeroSection';
 import GoButton from '../components/GoButton';
-import AdBanner from '../components/AdBanner'; // Import the AdBanner component
-import StackedCard from '../components/StackedCard'; // Import the StackedCard component
-import Offers from '../components/Offers'; // Import the Offers component
-import Locations from '../components/Locations'; // Import the Locations component
+import AdBanner from '../components/AdBanner';
+import StackedCard from '../components/StackedCard';
+import Offers from '../components/Offers';
+import Locations from '../components/Locations';
 import HeaderContainer from '../components/HeaderContainer';
-import LoginSignup from '../components/LoginSignup';
-import SearchCard from '../components/SearchBar';
+import RecommendAndMostViewed, { recommendedHotelsList, mostViewedHotelsList } from '../components/RecommendAndMostViewed';
 
 function Home() {
   return (
     <div className="container">
       <HeroSection />
       <HeaderContainer />
-      <SearchCard />
       <SearchDisplay />
-      <div className="flex justify-center mt-8">
+      {/* <div className="flex justify-center mt-8">
         <GoButton />
-      </div>
+      </div> */}
       <AdBanner imageUrl="/path/to/ad-image.jpg" /> {/* Use the AdBanner component */}
       <StackedCard /> {/* Include the StackedCard component */}
+      <RecommendAndMostViewed title={'Recommended for you'} hotels={recommendedHotelsList}/> {/* Include the recommended section*/}
+      <RecommendAndMostViewed title={'Most Viewed'} hotels={mostViewedHotelsList}/> {/* Include the most viewed section*/}
       <Offers /> {/* Include the Offers component */}
       <Locations /> {/* Include the Locations component */}
     </div>
