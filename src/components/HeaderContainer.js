34d6d2d06635fb271d505bfd97629b26513fafc6
dhruv <dpatel7090@conestogac.on.ca>
2024-07-10 00:16:54 -0400

    added flights page, added page links, and SearchDisplay for flights

--- src/components/HeaderContainer.js
+++ src/components/HeaderContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import HotelsIcon from './icons/hotels.svg';
 import FlightsIcon from './icons/flights.svg';
 import HolidayPackagesIcon from './icons/holiday-packages.svg';
@@ -6,20 +7,20 @@ import HolidayPackagesIcon from './icons/holiday-packages.svg';
 const HeaderContainer = () => {
   return (
     <div className="header-container flex items-center w-[800px] h-[110px] rounded-3xl shadow-custom-shadow bg-white justify-between mt-[-350px] mx-auto webkit-sticky sticky z-50 top-5">
-      <div className="section hotels">
+      <Link to="/" className="section hotels">
         <img src={HotelsIcon} alt="Hotels" className="icon" />
         <p className="text">Hotels</p>
-      </div>
+      </Link>
       <div className="divider"></div>
-      <div className="section flights">
+      <Link to="/flights" className="section flights">
         <img src={FlightsIcon} alt="Flights" className="icon" />
         <p className="text">Flights</p>
-      </div>
+      </Link>
       <div className="divider"></div>
-      <div className="section holiday-packages">
+      <Link to="/holiday-packages" className="section holiday-packages">
         <img src={HolidayPackagesIcon} alt="Holiday Packages" className="icon" />
         <p className="text">Holiday Packages</p>
-      </div>
+      </Link>
     </div>
   );
 };
