5a5cf163517038954a46174f67b8d09967a35a36
Shrisayadesigns <shrisayadesigns@gmail.com>
2024-07-18 23:47:27 -0400

    added language translation

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,16 @@ import ProfileIcon from "./icons/profile.svg";
 import { Link } from "react-router-dom";
 import { useAuth } from "../utils/AuthContext";
 import LanguageIcon from '../components/icons/language.svg'; // Assuming you have a language icon
+import { useTranslation } from 'react-i18next';
 
 const Header = ({ onSignInClick }) => {
   const { isAuthenticated } = useAuth();
+  const { t, i18n } = useTranslation();
+
+  const handleLanguageChange = (e) => {
+    i18n.changeLanguage(e.target.value);
+  };
+
   return (
     <div className="header bg-[#002475] p-4 md:px-[80px] md:py-4 flex items-center justify-between">
       <Link to="/">
@@ -15,12 +22,15 @@ const Header = ({ onSignInClick }) => {
       <div className="flex items-center space-x-6">
         <div className="items-center space-x-2 cursor-pointer hidden md:flex">
           <img className="h-6 " src={LanguageIcon} alt="Language" />
-          <select className="bg-transparent text-white cursor-pointer">
-            <option value="en">English</option>
-            <option value="fr">French</option>
+          <select
+            className="bg-transparent text-white cursor-pointer"
+            onChange={handleLanguageChange}
+          >
+            <option value="en" className="text-black">English</option>
+            <option value="fr" className="text-black">French</option>
           </select>
         </div>
-        <Link to="/support" className="text-white font-semibold">Support</Link>
+        <Link to="/support" className="text-white font-semibold">{t('support')}</Link>
         {isAuthenticated ? (
           <div className="flex items-center">
             {localStorage.getItem("user") === "admin@gmail.com" && (
@@ -41,7 +51,7 @@ const Header = ({ onSignInClick }) => {
             className="text-white font-semibold cursor-pointer"
             onClick={onSignInClick}
           >
-            Sign In
+            {t('sign_in')}
           </span>
         )}
       </div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,8 +2,10 @@ import React, { useState } from 'react';
 import { Autocomplete, TextField, Slider, Typography } from '@mui/material';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import { useTranslation } from 'react-i18next';
 
 const SearchCard = () => {
+  const { t } = useTranslation();
   const [location, setLocation] = useState(null);
   const [checkInDate, setCheckInDate] = useState(null);
   const [checkOutDate, setCheckOutDate] = useState(null);
@@ -43,9 +45,9 @@ const SearchCard = () => {
   return (
     <div className="max-w-[1205px] h-[610px] md:h-[443px] xl:w-[100%] lg:w-[90%] md:w-[90%] mx-auto pt-15 md:pt-[4rem] px-6 pb-6 md:border-2 md:border-gray-300 md:rounded-[40px] bg-white md:shadow-md mt-[-65px] md:mt-[-40px] md:h-[410px] md:flex md:flex-col" style={{ boxShadow: '0px 12px 24px 0px rgba(0, 0, 0, 0.10)' }}>
       <div className="text-center mb-6">
-        <h2 className="text-4xl hidden md:block font-semibold text-[#008EC4]">Good Morning!</h2>
+        <h2 className="text-4xl hidden md:block font-semibold text-[#008EC4]">{t('good_morning')}</h2>
         <p className="text-sm md:text-lg text-gray-600">
-          Explore beautiful places in the world with{' '}
+          {t('explore_beautiful_places')} 
           <span className="font-bold">
             <span className="text-blue-900">book</span>
             <span className="text-red-700">n</span>
@@ -56,17 +58,17 @@ const SearchCard = () => {
         <div className="flex justify-center space-x-8 mt-4">
           <label className="inline-flex items-center md:ml-0">
             <input type="radio" className="form-radio text-blue-600" name="groupType" value="family" />
-            <span className="ml-2">Family</span>
+            <span className="ml-2">{t('family')}</span>
           </label>
           <label className="inline-flex items-center md:ml-0">
             <input type="radio" className="form-radio text-blue-600" name="groupType" value="group" />
-            <span className="ml-2">Group Deals</span>
+            <span className="ml-2">{t('group_deals')}</span>
           </label>
         </div>
       </div>
       <div className="w-[360px] h-[auto] xl:w-[100%] lg:w-[100%] md:w-[95%] flex-shrink-0 border-2 border-gray-300 rounded-xl md:border-2 md:border-gray-300 md:rounded-2xl mx-auto p-4 md:p-6 md:flex md:flex-row flex md:flex-nowrap flex-wrap justify-between items-start gap-4 md:gap-0">
         <div className="flex flex-col justify-between w-[calc(50%-10px)] md:w-[200px] lg:mx-2 md:mx-2 mb-4 md:mb-0">
-          <h4 className="text-[#606060] font-normal text-[16px] leading-[110%] md:text-[16px]">Location</h4>
+          <h4 className="text-[#606060] font-normal text-[16px] leading-[110%] md:text-[16px]">{t('location')}</h4>
           <Autocomplete
             value={location}
             onChange={handleLocationChange}
@@ -78,7 +80,7 @@ const SearchCard = () => {
                 variant="standard"
                 InputProps={{ ...params.InputProps, disableUnderline: true }}
                 className="text-[#000] leading-trim text-edge-cap font-inter text-[30px] font-bold leading-[110%] w-full"
-                placeholder="Select Location"
+                placeholder={t('select_location')}
               />
             )}
             classes={{
@@ -91,12 +93,12 @@ const SearchCard = () => {
         <div className="w-[2px] h-[60px] md:w-[3px] md:h-[92px] bg-gray-300 flex-shrink-0 hidden md:block"></div>
 
         <div className="flex flex-col justify-between w-[calc(50%-10px)] md:w-[200px] xl:mx-auto lg:mx-2 md:mx-2 mb-4 md:mb-0">
-          <h4 className="text-[#606060] font-normal text-[16px] leading-[110%] md:text-[16px]">Check-In</h4>
+          <h4 className="text-[#606060] font-normal text-[16px] leading-[110%] md:text-[16px]">{t('check_in')}</h4>
           <DatePicker
             selected={checkInDate}
             onChange={(date) => setCheckInDate(date)}
             dateFormat="dd MMM yyyy"
-            placeholderText="Select Date"
+            placeholderText={t('select_date')}
             className="text-[#000] leading-trim text-edge-cap text-[24px] xl:text-[30px] lg:text-[25px] md:text-[18px] font-bold leading-[110%] w-full"
           />
           <Typography variant="body2" className="text-[#606060] font-normal text-[20px] leading-[110%]">Thursday</Typography>
@@ -105,12 +107,12 @@ const SearchCard = () => {
         <div className="w-[2px] h-[60px] md:w-[3px] md:h-[92px] bg-gray-300 flex-shrink-0 hidden md:block"></div>
 
         <div className="flex flex-col justify-between w-[calc(50%-10px)] md:w-[200px] xl:mx-auto lg:mx-2 md:mx-2 mb-4 md:mb-0">
-          <h4 className="text-[#606060] font-normal text-[16px] leading-[110%] md:text-[16px]">Check-Out</h4>
+          <h4 className="text-[#606060] font-normal text-[16px] leading-[110%] md:text-[16px]">{t('check_out')}</h4>
           <DatePicker
             selected={checkOutDate}
             onChange={(date) => setCheckOutDate(date)}
             dateFormat="dd MMM yyyy"
-            placeholderText="Select Date"
+            placeholderText={t('select_date')}
             className="text-[#000] leading-trim text-edge-cap text-[24px] xl:text-[30px] lg:text-[25px] md:text-[18px] font-bold leading-[110%] w-full"
           />
           <Typography variant="body2" className="text-[#606060] font-normal text-[20px] leading-[110%]">Friday</Typography>
@@ -119,7 +121,7 @@ const SearchCard = () => {
         <div className="w-[2px] h-[60px] md:w-[3px] md:h-[92px] bg-gray-300 flex-shrink-0 hidden md:block"></div>
 
         <div className="flex flex-col justify-between w-[calc(50%-10px)] md:w-[200px] xl:mx-auto lg:mx-2 md:mx-2 mb-4 md:mb-0">
-          <h4 className="text-[#606060] font-normal text-[16px] leading-[110%] md:text-[16px]">Rooms & Guests</h4>
+          <h4 className="text-[#606060] font-normal text-[16px] leading-[110%] md:text-[16px]">{t('rooms_and_guests')}</h4>
           <Autocomplete
             value={roomsAndGuests}
             onChange={handleRoomsAndGuestsChange}
@@ -131,7 +133,7 @@ const SearchCard = () => {
                 variant="standard"
                 InputProps={{ ...params.InputProps, disableUnderline: true }}
                 className="text-[#000] leading-trim text-edge-cap font-inter text-[30px] font-bold leading-[110%] w-full"
-                placeholder="Select Rooms & Guests"
+                placeholder={t('select_rooms_guests')}
               />
             )}
             classes={{
@@ -143,7 +145,7 @@ const SearchCard = () => {
         <div className="w-[2px] h-[60px] md:w-[3px] md:h-[92px] bg-gray-300 flex-shrink-0 hidden md:block"></div>
 
         <div className="flex flex-col justify-between w-[calc(50%-10px)] md:w-[200px] xl:mx-auto lg:mx-2 md:mx-2 mb-4 md:mb-0">
-          <h4 className="text-[#606060] font-normal text-[16px] leading-[110%] md:text-[16px]">Price</h4>
+          <h4 className="text-[#606060] font-normal text-[16px] leading-[110%] md:text-[16px]">{t('price')}</h4>
           <Slider
             value={priceRange}
             onChange={handlePriceChange}
