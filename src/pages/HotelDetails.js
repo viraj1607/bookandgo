acf1e643142e58fc4b7bd51945bc3161ab36936f
viraj1607 <merai.viraj@gmail.com>
2024-07-24 12:26:31 -0400

    get hotel details

--- src/pages/HotelDetails.js
+++ src/pages/HotelDetails.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import HotelInfo from '../components/HotelInfo';
 import { useLocation } from 'react-router-dom';
+import { getHotelDetails } from '../custom-functions/getHotelDetails';
 
 const HotelDetails = () => {
   const { state } = useLocation();
   const { hotelData } = state || {};
+
+  useEffect(()=>{
+      getHotelDetails("6696c3a14b2414bee03eebc1")
+  },[])
   return (
     <div>
       {hotelData ? <HotelInfo hotelData={hotelData} /> : <p>No hotel data available.</p>}
