60ab186453bb94ed2af6a72f0774501622b6399e
viraj1607 <merai.viraj@gmail.com>
2024-08-07 21:03:40 -0400

    flight page fixed and random hotel list api integrated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SearchDisplay from "../components/SearchDisplay";
 import HeroSection from "../components/HeroSection";
 import GoButton from "../components/GoButton";
@@ -9,8 +9,19 @@ import Locations from "../components/Locations";
 import HeaderContainer from "../components/HeaderContainer";
 import SearchCard from "../components/SearchBar";
 import SignInPopup from "../components/SignInPopup";
+import { getRandomHotels } from "../custom-functions/getRandomHotels";
 
 function Home() {
+  const [randomHotels, setRandomHotels] = useState([]);
+
+  useEffect(() => {
+    const fetchRandomHotels = async () => {
+      const data = await getRandomHotels();
+      setRandomHotels(data);
+    };
+
+    fetchRandomHotels();
+  }, []);
   return (
     <div className="container relative">
       <SignInPopup />
@@ -18,7 +29,9 @@ function Home() {
       <HeaderContainer />
       <SearchCard />
       {/* <GoButton /> */}
-      <SearchDisplay />
+      {randomHotels.length > 0 && (
+        <SearchDisplay randomHotelsData={randomHotels} />
+      )}
       <AdBanner1 imageUrl="/path/to/ad-image.jpg" />
       <StackedCard />
       <Offers />
