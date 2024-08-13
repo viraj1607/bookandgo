a22d19436140fa11719bd17fb41a753a7ef544e4
viraj1607 <merai.viraj@gmail.com>
2024-08-12 23:39:03 -0400

    server url

--- src/custom-functions/getRandomHotels.js
+++ src/custom-functions/getRandomHotels.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getRandomHotels = async () => {
   try {
     const response = await axios.get(
-      "https://bookngo-server-git-main-viraj1607s-projects.vercel.app//api/hotels/random/Toronto",
+      "https://bookngo-server-git-main-viraj1607s-projects.vercel.app/api/hotels/random/Toronto",
       {}
     );
     console.log(response.data);
